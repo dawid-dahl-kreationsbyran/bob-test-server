8a9248f332b7f60eab6bae374a1329e72c75d247
Dawid-Dahl <dawid.dahl@kreationsbyran.se>
2022-03-13 17:50:13 +0100

    added errorhandling

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,32 @@
 const express = require("express")
 const router = express.Router()
 
-const database = [{ id: 1, name: "test-name" }]
+const database = [{ id: 1, employee_name: "test-name" }]
 
 router.get("/", (req, res, next) => {
-	res.send("API -> GETTING RESOURCE")
+	return res
+		.status(200)
+		.json({ success: true, data: "data from GET endpoint" })
 })
 
 router.post("/", (req, res, next) => {
 	console.log("req.body: ", req.body)
 
+	const hasKeys = obj => !!Object.keys(obj).length
+
+	if (!hasKeys(req.body)) {
+		return res
+			.status(422)
+			.json({ success: true, data: "incorrect payload" })
+	}
+
 	database.push({ id: Math.floor(Math.random() * 100000), ...req.body })
 
 	console.log("DB: ", database)
 
-	res.status(200).json({ success: true, data: "data from endpoint" })
+	return res
+		.status(200)
+		.json({ success: true, data: "data from POST endpoint" })
 })
 
 module.exports = router
